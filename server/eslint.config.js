f184f1bc5b1be0f81fdab26be2460d5f887a296c
coderaiser <coderaiser@cloudcmd.io>
2026-07-11 23:44:51 +0000

    chore: @putout/editor-server: forbid explicit any

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,10 +1,14 @@
 import {safeAlign} from 'eslint-plugin-putout';
 import {defineConfig} from 'eslint/config';
+import {matchToFlat} from '@putout/eslint-flat';
 
-export default defineConfig([
-    safeAlign, {
-        rules: {
-            '@typescript-eslint/no-unused-vars': 'off',
-        },
+export const match = {
+    '**/*.ts': {
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-explicit-any': 'error',
     },
+};
+export default defineConfig([
+    safeAlign,
+    ...matchToFlat(match),
 ]);
