957292d9c03ba5a8fc145682bd3d842087cee5df
coderiaser <coderaiser@cloudcmd.io>
2026-07-11 13:11:45 +0300

    chore: client apply try-catch

--- client/src/components/LocalStorage.js
+++ client/src/components/LocalStorage.js
@@ -5,18 +5,19 @@ const key = 'explorerSettingsV1';
 const noop = () => {};
 
 export const writeState = storage ? (state) => {
-    const [error] = tryCatch(storage.setItem.bind(storage), key, JSON.stringify(state));
+    const setItem = storage.setItem.bind(storage);
+    const [error] = tryCatch(setItem, key, JSON.stringify(state));
     
     error && console.warn('Unable to write to local storage.');
 } : noop;
 
-export const readState = storage ? () => {
-    try {
-        const state = storage.getItem(key);
-        
-        if (state)
-            return JSON.parse(state);
-    } catch {
-        console.warn('Unable to read from local storage.');
-    }
-} : noop;
+export const readState = !storage ? noop : () => {
+    const getItem = storage.getItem.bind(storage);
+    const [error, state] = tryCatch(getItem, key);
+    
+    if (error)
+        return console.warn('Unable to read from local storage.');
+    
+    if (state)
+        return JSON.parse(state);
+};
