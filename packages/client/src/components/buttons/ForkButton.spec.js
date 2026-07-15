86770cbb5d580ffb2d23775e3b5bb66af10b9d27
coderaiser <coderaiser@cloudcmd.io>
2026-07-15 06:48:46 +0000

    test: client: add ForkButton spec

---
+++ packages/client/src/components/buttons/ForkButton.spec.js
@@ -0,0 +1,62 @@
+import {test} from 'supertape';
+import {render, cleanup, fireEvent} from '@testing-library/react';
+import ForkButton from './ForkButton.js';
+
+test('ForkButton: disabled when canFork is false', (t) => {
+    render(<ForkButton canFork={false} saving={false} forking={false}/>);
+    
+    const button = document.querySelector('button');
+    
+    cleanup();
+    
+    t.ok(button.disabled);
+    t.end();
+});
+
+test('ForkButton: disabled when saving', (t) => {
+    render(<ForkButton canFork={true} saving={true} forking={false}/>);
+    
+    const button = document.querySelector('button');
+    
+    cleanup();
+    
+    t.ok(button.disabled);
+    t.end();
+});
+
+test('ForkButton: disabled when forking', (t) => {
+    render(<ForkButton canFork={true} saving={false} forking={true}/>);
+    
+    const button = document.querySelector('button');
+    
+    cleanup();
+    
+    t.ok(button.disabled);
+    t.end();
+});
+
+test('ForkButton: enabled when canFork true and not busy', (t) => {
+    render(<ForkButton canFork={true} saving={false} forking={false}/>);
+    
+    const button = document.querySelector('button');
+    
+    cleanup();
+    
+    t.notOk(button.disabled);
+    t.end();
+});
+
+test('ForkButton: onFork called on click', (t) => {
+    let called = false;
+    const onFork = () => {
+        called = true;
+    };
+    
+    render(<ForkButton canFork={true} saving={false} forking={false} onFork={onFork}/>);
+    
+    fireEvent.click(document.querySelector('button'));
+    cleanup();
+    
+    t.ok(called);
+    t.end();
+});
