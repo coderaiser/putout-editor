2c13b5d4598ae6b0a0260d1ff50b0a94bb0aa4cc
coderaiser <coderaiser@users.noreply.github.com>
2026-07-15 06:01:42 +0000

    chore: putout-editor: actions: lint ☘️

--- packages/client/src/utils/stringify.spec.js
+++ packages/client/src/utils/stringify.spec.js
@@ -1,15 +1,10 @@
 import {test} from 'supertape';
 import stringify from './stringify.js';
 
-test('stringify: function returns signature', (t) => {
-    const result = stringify(function example(a, b) {});
-    
-    t.equal(result, 'function example(a, b)');
-    t.end();
-});
-
 test('stringify: object', (t) => {
-    const result = stringify({a: 1});
+    const result = stringify({
+        a: 1,
+    });
     
     t.equal(result, String.raw`"\"\""`);
     t.end();
