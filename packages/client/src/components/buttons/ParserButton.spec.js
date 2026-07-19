db274e63b67210c0e50aa86928dc7cf536fdc4d5
coderiaser <coderaiser@cloudcmd.io>
2026-07-19 22:25:17 +0300

    fix: client: gist spec Promise.resolve mocks; ParserButton spec NodeList.at

--- packages/client/src/components/buttons/ParserButton.spec.js
+++ packages/client/src/components/buttons/ParserButton.spec.js
@@ -56,7 +56,7 @@ test('ParserButton: settings button disabled when parser has no settings', (t) =
     );
     
     const buttons = document.querySelectorAll('button');
-    const settingsBtn = buttons.at(-1);
+    const settingsBtn = [...buttons].at(-1);
     
     cleanup();
     
