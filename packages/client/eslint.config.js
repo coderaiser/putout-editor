3efe5988dbc401df20ec98b27d9c6cb5ab534ab2
coderiaser <coderaiser@cloudcmd.io>
2026-07-21 14:59:04 +0300

    chore: @putout/editor: client: react compiler

--- packages/client/eslint.config.js
+++ packages/client/eslint.config.js
@@ -1,10 +1,14 @@
 import {safeAlign} from 'eslint-plugin-putout';
 import {defineConfig} from 'eslint/config';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default defineConfig([
-    safeAlign, {
+    safeAlign,
+    {
+        plugins: {'react-compiler': reactCompiler},
         rules: {
             'no-irregular-whitespace': 'off',
+            'react-compiler/react-compiler': 'error',
             'n/no-unsupported-features/node-builtins': 'off',
             'putout/no-unresolved': 'off',
         },
