8bae90bfb0954851315c985237ddfaebba30ba91
coderaiser <coderaiser@cloudcmd.io>
2026-08-30 13:41:43 +0000

    refactor: SettingsDialog: convert to functional component

--- packages/client/src/components/dialogs/SettingsDialog.js
+++ packages/client/src/components/dialogs/SettingsDialog.js
@@ -1,74 +1,57 @@
+import {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 
-export default class SettingsDialog extends React.Component {
-    constructor(props) {
-        super(props);
-        this._outerClick = this._outerClick.bind(this);
-        this._onChange = this._onChange.bind(this);
-        this._reset = this._reset.bind(this);
-        this._saveAndClose = this._saveAndClose.bind(this);
-        this.state = {
-            parserSettings: this.props.parserSettings,
-        };
-    }
+export default function SettingsDialog({visible, parser, parserSettings, onSave, onWantToClose}) {
+    const [localSettings, setLocalSettings] = useState(parserSettings);
     
-    UNSAFE_componentWillReceiveProps(nextProps) {
-        this.setState({
-            parserSettings: nextProps.parserSettings,
-        });
-    }
+    useEffect(() => {
+        setLocalSettings(parserSettings);
+    }, [parserSettings]);
     
-    _outerClick(event) {
+    function handleOuterClick(event) {
         if (event.target === document.getElementById('SettingsDialog'))
-            this._saveAndClose();
+            handleSaveAndClose();
     }
     
-    _onChange(parserSettings) {
-        this.setState({
-            parserSettings,
-        });
+    function handleChange(newSettings) {
+        setLocalSettings(newSettings);
     }
     
-    _saveAndClose() {
-        this.props.onSave(this.props.parser, this.state.parserSettings);
-        this.props.onWantToClose();
+    function handleSaveAndClose() {
+        onSave(parser, localSettings);
+        onWantToClose();
     }
     
-    _reset() {
-        this.setState({
-            parserSettings: {},
-        });
+    function handleReset() {
+        setLocalSettings({});
     }
     
-    render() {
-        if (this.props.visible && this.props.parser.renderSettings)
-            return (
-                <div id="SettingsDialog" className="dialog" onClick={this._outerClick}>
-                    <div className="inner">
-                        <div className="header">
-                            <h3>{this.props.parser.displayName} Settings</h3>
-                        </div>
-                        <div className="body">
-                            {this.props.parser.renderSettings(this.state.parserSettings, this._onChange)}
-                        </div>
-                        <div className="footer">
-                            <button
-                                style={{
-                                    marginRight: 10,
-                                }}
-                                onClick={this._reset}
-                            >
-                                Reset
-                            </button>
-                            <button onClick={this._saveAndClose}>Close</button>
-                        </div>
+    if (visible && parser.renderSettings)
+        return (
+            <div id="SettingsDialog" className="dialog" onClick={handleOuterClick}>
+                <div className="inner">
+                    <div className="header">
+                        <h3>{parser.displayName} Settings</h3>
+                    </div>
+                    <div className="body">
+                        {parser.renderSettings(localSettings, handleChange)}
+                    </div>
+                    <div className="footer">
+                        <button
+                            style={{
+                                marginRight: 10,
+                            }}
+                            onClick={handleReset}
+                        >
+                            Reset
+                        </button>
+                        <button onClick={handleSaveAndClose}>Close</button>
                     </div>
                 </div>
-            );
-        
-        return null;
-    }
+            </div>
+        );
+    
+    return null;
 }
 
 SettingsDialog.propTypes = {
