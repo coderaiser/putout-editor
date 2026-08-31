2b35fd0b1dca391248ef5d41f8d43938ab1ba40a
coderaiser <coderaiser@users.noreply.github.com>
2026-08-31 22:52:14 +0000

    chore: putout-editor: actions: lint ☘️

--- packages/client/src/transform/format.ts
+++ packages/client/src/transform/format.ts
@@ -4,21 +4,27 @@ import {normalizeRule} from './normalizeRule.ts';
 
 export const formatInput = async (source: string, ast: Node) => {
     if (!ast)
-        return [Error('No AST')];
+        return [
+            Error('No AST'),
+        ];
     
     const {print} = await import('@putout/printer');
     
     const formatted = print(ast);
     
     if (formatted === source)
-        return [Error('No changes')];
+        return [
+            Error('No changes'),
+        ];
     
     return [null, formatted];
 };
 
 export const formatRule = async (source: string) => {
     if (!source)
-        return [Error('No source')];
+        return [
+            Error('No source'),
+        ];
     
     const {parse} = await import('@babel/parser');
     const {default: plugins} = await import('@putout/engine-parser/babel/plugins');
