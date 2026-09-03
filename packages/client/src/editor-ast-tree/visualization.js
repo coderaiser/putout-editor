44266b5474e8c6de3670902d58eaba1299294ecf
coderaiser <coderaiser@cloudcmd.io>
2026-09-03 20:38:52 +0000

    fix(visualization): add displayName for proper toolbar labeling
    - Add tests for visualization naming (TDD)
    - Add displayName property to Tree and EditorASTJson
    - Ensures toolbar shows correct names instead of garbage
    
    scope: editor-ast-tree

--- packages/client/src/editor-ast-tree/visualization.js
+++ packages/client/src/editor-ast-tree/visualization.js
@@ -1,4 +1,7 @@
 import EditorASTJson from '#editor-ast-json';
 import Tree from './Tree.js';
 
+Tree.displayName = 'Tree';
+EditorASTJson.displayName = 'JSON';
+
 export default [Tree, EditorASTJson];
--- packages/client/src/editor-ast-tree/visualization.spec.js
+++ packages/client/src/editor-ast-tree/visualization.spec.js
@@ -12,3 +12,13 @@ test('visualization: index: includes 2 visualizations', (t) => {
     t.equal(visualizations.length, 2);
     t.end();
 });
+
+test('visualization: Tree has name property', (t) => {
+    t.equal(visualizations[0].displayName, 'Tree', 'Tree visualization should have displayName');
+    t.end();
+});
+
+test('visualization: EditorASTJson has name property', (t) => {
+    t.equal(visualizations[1].displayName, 'JSON', 'EditorASTJson visualization should have displayName');
+    t.end();
+});
