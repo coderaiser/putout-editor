d82e90182758010d7aaaf86ce87c3cf909b6a9df
coderaiser <coderaiser@cloudcmd.io>
2026-09-14 06:05:32 +0000

    test: client: e2e: syntax error

--- packages/client/e2e/mobile.ts
+++ packages/client/e2e/mobile.ts
@@ -179,7 +179,7 @@ test('updating transform editor changes code output', async ({page}) => {
     const {write, press} = await editor.get(EDITOR_TRANSFORM);
     await press('i');
     await press('ControlOrMeta+A');
-    await write(`export const replace = () => ({'"use strict": ''});`);
+    await write(`export const replace = () => ({'\"use strict\"': ''});`);
     await page.waitForTimeout(400);
     
     await page
