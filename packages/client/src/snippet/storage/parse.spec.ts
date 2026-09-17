d3821d94299afbea0b2773536cf37015e3313f44
coderaiser <coderaiser@cloudcmd.io>
2026-09-17 07:30:11 +0000

    test: client: snippet: coverage

--- packages/client/src/snippet/storage/parse.spec.ts
+++ packages/client/src/snippet/storage/parse.spec.ts
@@ -86,7 +86,7 @@ test('parse: fetchFromURL: resolves Revision when hash is valid', async (t) => {
     });
     server.use(...parseHandlers);
     
-    const result = await withHash('#/abc123', stub().resolves(await fetchFromURL()));
+    const result = await withHash('#/abc123', async () => fetchFromURL());
     
     server.close();
     
